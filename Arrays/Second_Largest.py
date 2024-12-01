bad8530eaeb08649682dd7b530e99a98f81676db
Balarama Vamsi <161118315+vamsi-op@users.noreply.github.com>
2024-12-01 08:58:13 +0530

    Added code to Second_Largest.py

--- Arrays/Second_Largest.py
+++ Arrays/Second_Largest.py
@@ -1,1 +1,18 @@
-
+#test cases
+t = int(input())
+for i in range(t):
+  arr = list(map(int, input().split()))
+  n = len(arr)
+  largest = -1
+  secondLargest = -1
+  for i in range(n):
+        # If arr[i] > largest, update second largest with
+        # largest and largest with arr[i]
+        if arr[i] > largest:
+            secondLargest = largest
+            largest = arr[i]
+        # If arr[i] < largest and arr[i] > second largest, 
+        # update second largest with arr[i]
+        elif arr[i] < largest and arr[i] > secondLargest:
+            secondLargest = arr[i]
+  print(secondLargest)
