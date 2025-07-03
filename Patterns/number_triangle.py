4a96175471ce72fb51c0a4d3109070046f273eff
Balarama Vamsi <161118315+vamsi-op@users.noreply.github.com>
2025-07-03 21:20:50 +0530

    Create number_triangle.py

---
+++ Patterns/number_triangle.py
@@ -0,0 +1,14 @@
+# Number Triangle pattern
+
+def pat(n):
+  for i in range(n):
+    for j in range(i + 1):
+      print(f"{j + 1} ", end="") # end argument allows us skip new line
+    print() # move to new line
+
+for _ in range(int(input("Enter number of test cases: "))):
+  try:
+    n = int(input("Enter number of rows: "))
+    pat(n)
+  except ValueError:
+    print("Enter a valid number")
