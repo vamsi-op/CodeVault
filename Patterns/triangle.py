20c6d34dd7bb48d270551804283efa50077674ce
Balarama Vamsi <161118315+vamsi-op@users.noreply.github.com>
2025-07-03 21:18:04 +0530

    Update and rename Triangle.py to triangle.py

--- Patterns/triangle.py
+++ Patterns/triangle.py
@@ -3,8 +3,8 @@
 def pat(n):
   for i in range(n):
     for j in range(i + 1):
-      print("# ", end="")
-    print()
+      print("# ", end="") # end argument allows us skip new line
+    print() # move to new line
 
 for _ in range(int(input("Enter number of test cases: "))):
   try:
