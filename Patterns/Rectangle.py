d593e850263241986d7b866fd2efc9c2732c20ab
Balarama Vamsi <161118315+vamsi-op@users.noreply.github.com>
2025-07-03 21:01:31 +0530

    Create Rectangle.py

---
+++ Patterns/Rectangle.py
@@ -0,0 +1,10 @@
+# print rectangle pattern
+def pat(x, y):
+  for i in range(x):
+    for j in range(y):
+      print('# ', end="") # end argument allows us skip new line
+    print() # move to new line
+
+x = int(input("Enter rows count: "))
+y = int(input("Enter columns count: "))
+pat(x, y)
