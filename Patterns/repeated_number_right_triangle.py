e0c2b0f78a972d65fa0a6b1c5ad4bb809315b781
Balarama Vamsi <161118315+vamsi-op@users.noreply.github.com>
2025-07-05 11:16:01 +0530

    Create repeated_number_right_triangle.py

---
+++ Patterns/repeated_number_right_triangle.py
@@ -0,0 +1,15 @@
+# Repeated Number Right Triangle pattern
+
+def pat(n):
+  for i in range(n):
+    for j in range(i + 1):
+      # print same value
+      print(f"{i + 1} ", end="") # end argument allows us skip new line
+    print() # move to new line
+
+for _ in range(int(input("Enter number of test cases: "))):
+  try:
+    n = int(input("Enter number of rows: "))
+    pat(n)
+  except ValueError:
+    print("Enter a valid number")
