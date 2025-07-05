5dac209a8cba1df222d434287de932096892e76f
Balarama Vamsi <161118315+vamsi-op@users.noreply.github.com>
2025-07-05 14:31:43 +0530

    Create inverted_pyramid.py

---
+++ Patterns/inverted_pyramid.py
@@ -0,0 +1,19 @@
+# Inverted Pyramid Pattern
+
+def pat(n):
+    for i in range(n, 0, -1):
+        # print blanks
+        for j in range(n - i):
+           print(" ", end="")
+        # print '#'
+        for j in range(i):
+            print("# ", end="")
+        print() # new line
+    
+
+for _ in range(int(input("Enter number of test cases: "))):
+  try:
+    n = int(input("Enter number of rows: "))
+    pat(n)
+  except ValueError:
+    print("Enter a valid number")
