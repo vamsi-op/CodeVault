5c51d5242d5f821fb49fc88652fff3b9479a4299
Balarama Vamsi <161118315+vamsi-op@users.noreply.github.com>
2025-07-05 19:12:36 +0530

    Update binary_triangle.py

--- Patterns/binary_triangle.py
+++ Patterns/binary_triangle.py
@@ -1,1 +1,17 @@
+# Binary Triangle Pattern
 
+def pat(n):
+    for i in range(n):
+       for j in range(i + 1):
+          if (i + j) % 2 == 0:
+             print("1 ", end="")
+          else:
+             print("0 ", end="")
+       print()
+    
+for _ in range(int(input("Enter number of test cases: "))):
+  try:
+    n = int(input("Enter number of rows: "))
+    pat(n)
+  except ValueError:
+    print("Enter a valid number")
