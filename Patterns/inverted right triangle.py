5fc534c990b4d20f1e9bd695fa0f0fe1b2fe2f9e
Balarama Vamsi <161118315+vamsi-op@users.noreply.github.com>
2025-07-05 11:23:19 +0530

    Create inverted right triangle.py

---
+++ Patterns/inverted right triangle.py
@@ -0,0 +1,15 @@
+# Inverted Right Triangle pattern
+
+def pat(n):
+  # decrement loop
+  for i in range(n, 0, -1):
+    for j in range(i):
+      print("# ", end="") # end argument allows us skip new line
+    print() # move to new line
+
+for _ in range(int(input("Enter number of test cases: "))):
+  try:
+    n = int(input("Enter number of rows: "))
+    pat(n)
+  except ValueError:
+    print("Enter a valid number")
