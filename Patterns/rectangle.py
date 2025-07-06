9497fe1f0ec4fe4899e087ec58cca02105f4ce0d
Balarama Vamsi <161118315+vamsi-op@users.noreply.github.com>
2025-07-06 13:49:57 +0530

    Update rectangle.py

--- Patterns/rectangle.py
+++ Patterns/rectangle.py
@@ -1,13 +1,14 @@
 # print rectangle pattern
 def pat(x, y):
-  for i in range(x):
-    for j in range(y):
-      print('# ', end="") # end argument allows us skip new line
-    print() # move to new line
+    for i in range(x):
+        for j in range(y):
+            print('# ', end="") # end argument allows us skip new line
+        print() # move to new line
 
-try:
-  x = int(input("Enter rows count: "))
-  y = int(input("Enter columns count: "))
-  pat(x, y)
-except ValueError:
-  print("Enter valid number")
+for _ in range(int(input("Enter number of test cases: "))):
+    try:
+        x = int(input("Enter rows count: "))
+        y = int(input("Enter columns count: "))
+        pat(x, y)
+    except ValueError:
+        print("Enter valid number")
