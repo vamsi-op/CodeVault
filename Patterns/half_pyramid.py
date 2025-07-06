36c5315e3b576188bef769a3547defc8e73fa39b
Balarama Vamsi <161118315+vamsi-op@users.noreply.github.com>
2025-07-06 13:58:05 +0530

    Update half_pyramid.py

--- Patterns/half_pyramid.py
+++ Patterns/half_pyramid.py
@@ -1,14 +1,14 @@
-# Right Triangle pattern
+# Half pyramid pattern
 
 def pat(n):
-  for i in range(n):
-    for j in range(i + 1):
-      print("# ", end="") # end argument allows us skip new line
+    for i in range(n):
+        for j in range(i + 1):
+            print("# ", end="") # end argument allows us skip new line
     print() # move to new line
 
 for _ in range(int(input("Enter number of test cases: "))):
-  try:
-    n = int(input("Enter number of rows: "))
-    pat(n)
-  except ValueError:
-    print("Enter a valid number")
+    try:
+        n = int(input("Enter number of rows: "))
+        pat(n)
+    except ValueError:
+        print("Enter a valid number")
