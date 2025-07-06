3cb5433dd66e26769b1d5b9fec77024206208f4d
Balarama Vamsi <161118315+vamsi-op@users.noreply.github.com>
2025-07-06 13:44:32 +0530

    Create half_butterfly.py

---
+++ Patterns/half_butterfly.py
@@ -0,0 +1,21 @@
+# Half Butterfly Pattern
+
+def pat(n):
+    for i in range(1, n+1):
+        # print left half of the butterfly
+        for j in range(i):
+            print(f"{j+1} ", end="")
+        # print spaces
+        for j in range(2 * (n - i)):
+            print("  ", end="")
+        # print right half of the butterfly
+        for j in range(i):
+            print(f"{j + 1} ", end="")
+        print() # print new line
+    
+for _ in range(int(input("Enter number of test cases: "))):
+  try:
+    n = int(input("Enter number of rows: "))
+    pat(n)
+  except ValueError:
+    print("Enter a valid number")
