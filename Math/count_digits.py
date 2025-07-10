5996214330d2bb8cf5e9a997a538b015084eaee6
Balarama Vamsi <161118315+vamsi-op@users.noreply.github.com>
2025-07-10 18:54:35 +0530

    Update count_digits.py

--- Math/count_digits.py
+++ Math/count_digits.py
@@ -1,7 +1,11 @@
 for _ in range(int(input("Enter number of test cases: "))):
-    n = int(input("N: "))
-    res = 1
-    while n > 9:
-        res += 1
-        n //= 10
-    print(res)
+    try:
+        n = int(input("N: "))
+        res = 1
+        while n > 9:
+            res += 1
+            n //= 10
+        print(res)
+
+    except ValueError:
+        print("Enter a valid number")
